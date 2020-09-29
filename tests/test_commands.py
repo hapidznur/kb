7e38d7da5dadcfc50f22dec96b61d21cd5106e64
hafid <hapidznur@gmail.com>
2020-09-30 01:38:48 +0700

    fix linter

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,19 +1,17 @@
-import unittest
 from kb.commands import add
 from kb.config import DEFAULT_CONFIG
-from kb.main import main
 from pathlib import Path
 
 
 def test_add_check_file_created_on_destination():
     add.add(args={
-        "title":"test_title", # required
-        "file":"", # required
-        "category":"Journey,Notes",
-        "tags":"",
-        "author":"jhon doe",
-        "status":"",
-        "body":"",
+        "title": "test_title",  # required
+        "file": "",  # required
+        "category": "Journey,Notes",
+        "tags": "",
+        "author": "jhon doe",
+        "status": "",
+        "body": "",
         "template": "blog"}, config=DEFAULT_CONFIG)
     want = "~/.kb/default/test_title"
     got = str(Path(want))
